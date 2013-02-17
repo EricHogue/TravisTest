437b6dcc013f4eeb6a6cc0894fdf226df0237e3a
Eric Hogue <eric@erichogue.ca>
2013-02-17 17:24:45 -0500

    Fixed test

--- tests/FirstTest.php
+++ tests/FirstTest.php
@@ -6,7 +6,7 @@ class FirstTest extends \PHPUnit_Framework_TestCase {
 	/** @test */
 	public function blah() {
 		//$mock = mock::mock('stdClass');
-		$this->assertTrue(false);
+		$this->assertTrue(true);
 	}
 }
 
